e9e2bd89c861711cfaa6d884d14495e559b6ec2e
666immortal <2016222042@email.szu.edu.cn>
2019-06-17 23:06:39 +0800

    Design Message 6_17 Lastest

--- Message.c
+++ Message.c
@@ -1,9 +1,10 @@
 #include "Message.h"
+#include <stdio.h>
 #include <string.h>
 
 Status setCommand(MsgEntity *cmd, CmdType doWhat, char *str, int flag)
 {
-    if(str == NULL)
+    if(str == NULL || cmd == NULL)
     {
         return FAILURE;
     }
@@ -17,7 +18,7 @@ Status setCommand(MsgEntity *cmd, CmdType doWhat, char *str, int flag)
 
 Status setDialogue(MsgEntity *dlg, int forWho, char *str, MsgFlag flag)
 {
-    if(str == NULL)
+    if(str == NULL || dlg == NULL)
     {
         return FAILURE;
     }
@@ -31,12 +32,104 @@ Status setDialogue(MsgEntity *dlg, int forWho, char *str, MsgFlag flag)
 
 Status configLoginEty(MsgEntity *entity, char *user, char * pwd)
 {
-    if(user == NULL || pwd == NULL)
+    if(user == NULL || pwd == NULL || entity == NULL)
     {
         return FAILURE;
     }
 
+    char jointStr[200];
+    sprintf(jointStr, "%s %s", user, pwd);
+    entity->object = CMD_LOGIN;
+    entity->flag = SEND_FLAG;
+    strcpy(entity->details, jointStr);
 
     return SUCCESSFUL;
 }
-Status configRegisterEty(MsgEntity *entity, char *user, char * pwd);
+
+Status configRegisterEty(MsgEntity *entity, char *user, char * pwd)
+{
+    if(user == NULL || pwd == NULL || entity == NULL)
+    {
+        return FAILURE;
+    }
+
+    char jointStr[200];
+    sprintf(jointStr, "%s %s", user, pwd);
+    entity->object = CMD_REGISTER;
+    entity->flag = SEND_FLAG;
+    strcpy(entity->details, jointStr);
+
+    return SUCCESSFUL;
+}
+
+Status configOnlineListEty(MsgEntity *entity, char **list, int num)
+{
+    if(list == NULL || entity == NULL)
+    {
+        return FAILURE;
+    }
+
+    char jointStr[200];
+    int i;
+
+    strcpy(jointStr, list[0]);
+    for(i = 1; i < num; i++)
+    {
+        strcat(jointStr, list[i]);
+    }
+
+    entity->object = CMD_GETLIST;
+    entity->flag = num;
+    strcpy(entity->details, jointStr);
+
+    return SUCCESSFUL;
+}
+
+Status configExitEty(MsgEntity *entity)
+{
+    if(entity == NULL)
+    {
+        return FAILURE;
+    }
+
+    entity->object = CMD_EXIT;
+    entity->flag = SEND_FLAG;
+    strcpy(entity->details, "");
+
+    return SUCCESSFUL;
+}
+
+Status analysisCmdEty(MsgEntity *entity, int *interestType, void *interestPoint)
+{
+    Status res = FAILURE;
+    int type = 0;
+    switch (entity->object)
+    {
+    case CMD_LOGIN:
+        /* code */
+        type = CMD_LOGIN;
+        res = SUCCESSFUL;
+        break;
+    case CMD_REGISTER:
+        /* code */
+        type = CMD_REGISTER;
+        res = SUCCESSFUL;
+        break;
+    case CMD_GETLIST:
+        /* code */
+        type = CMD_GETLIST;
+        break;
+    case CMD_EXIT:
+        /* code */
+        type = CMD_EXIT;
+        break;
+    default:
+        break;
+    }
+    
+    if(interestType != NULL)
+    {
+        *interestType = type;
+    }
+    return res;
+}
--- Message.h
+++ Message.h
@@ -8,8 +8,8 @@ typedef int Status;
 
 typedef enum
 {
-    SEND = 0,
-    RECV = 1
+    SEND_FLAG = 0,
+    RECV_FLAG = 1
 }MsgFlag;
 
 typedef enum
@@ -43,11 +43,17 @@ typedef struct
 Status setCommand(MsgEntity *cmd, CmdType doWhat, char *str, int flag);
 // 将消息配置在实体中
 Status setDialogue(MsgEntity *dlg, int forWho, char *str, MsgFlag flag);
-// 配置登陆实体
+// 配置登陆实体，客户端才用得到，服务器负责解析
 Status configLoginEty(MsgEntity *entity, char *user, char * pwd);
-// 配置注册实体
+// 配置注册实体，客户端才用得到，服务器负责解析
 Status configRegisterEty(MsgEntity *entity, char *user, char * pwd);
+// 配置在线用户列表实体，服务器才用得到，客户端负责解析，客户不能太多
+Status configOnlineListEty(MsgEntity *entity, char **list, int num);
+// 配置退出实体，客户端才用得到，服务器负责解析
+Status configExitEty(MsgEntity *entity);
 
+// 解析命令实体
+Status analysisCmdEty(MsgEntity *entity, int *interestType, void *interestPoint);
 
 Status clientLogin(MsgEntity *entity, char *user, char * pwd);
 Status clientRegister(MsgEntity *entity, char *user, char *pwd);
